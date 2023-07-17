e60e53e3aec57c32a6cd1274b108c927f9aa61bd
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-17 07:52:09 -0700

    Hello Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return - Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a[100] = "Programming is like building a multilingual puzzle \n";
+	puts(a);
+	return (0);
+}
