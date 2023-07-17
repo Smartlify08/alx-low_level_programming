f133b6fc8963140461a86364e44f3aee48d4113a
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-17 12:05:28 -0700

    Quote into stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 /**
- * main - Prints and that piece of art is useful"
- *        - Dora Korpar, 2015-10-19,
+ * main - Prints and that piece of art is useful"-Dora Korpar, 2015-10-19,
  *
  * Return: always 0
  */
 int main(void)
 {
+	fprintf(stderr, "and that piece of art is useful");
+	fprintf(stderr, "\"-Dora Korpar, 2015-10-19");
 	return (1);
 }
