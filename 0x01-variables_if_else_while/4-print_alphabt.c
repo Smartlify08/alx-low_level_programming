4dba589e75c2a77c28160766a45374afe5f5e251
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-17 19:33:04 -0700

    Chopped off letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Print all alphabets except
+ *        e and q
+ *
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if(letter == 'e')
+		{
+			continue;
+		}
+		else if(letter == 'q')
+		{
+			continue;
+		}
+		putchar(letter);
+
+	}
+	putchar ('\n');
+	return 0;
+}
