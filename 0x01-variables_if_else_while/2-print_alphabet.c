93f0860c6ff57eba5d576054cb9d0c9d4315ca8c
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-17 18:36:24 -0700

    Print Alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,13 +4,15 @@
  *
  * Return: always 0(success)
  */
-int main(void)
+int main (void)
 {
 	char letter;
 
 	for(letter = 'a'; letter <= 'z'; letter++)
 	{
-		putchar(letter);
+		putchar(letter);	
 	}
+
+	putchar('\n');
 	return (0);
 }
