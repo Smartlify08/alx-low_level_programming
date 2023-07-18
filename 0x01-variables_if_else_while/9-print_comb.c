60122bcc232b2a307686096746de635470b703de
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-18 12:55:53 -0700

    Print combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - print combinations of numbers
+*
+* Return: always 0(successful)
+*/
+int main(void)
+{
+	int i;
+
+	for (i=48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
