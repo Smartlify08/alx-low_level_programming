52725927c969fc5ce2df4d9469beb5dc225d1aa9
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-18 10:53:51 -0700

    Print base 16 characters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Print all base 16 numbers 0-f
+ *
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	int hexNumber, hexLetter;
+	for (hexNumber = 48; hexNumber <= 57; hexNumber++)
+	{
+		putchar(hexNumber);
+	}
+	for (hexLetter = 97; hexLetter <= 102; hexLetter++)
+	{
+		putchar(hexLetter);
+	}
+	putchar('\n');
+	return(0);
+}
