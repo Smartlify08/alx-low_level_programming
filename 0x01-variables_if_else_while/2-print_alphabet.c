b874ad23326023e561df26d5968de1685495035e
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-17 18:20:39 -0700

    Print Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print letters of the alphabet
+ *
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	char letter;
+
+	for(letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	return (0);
+}
