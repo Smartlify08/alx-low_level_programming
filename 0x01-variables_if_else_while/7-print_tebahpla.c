783534be87dae194e6b02bd8823316a511e6bd6e
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-18 04:36:14 -0700

    Reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print alphabets in reversed form
+ *
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	int alphaValue;
+	for (alphaValue = 122; alphaValue > 96; alphaValue--)
+	{
+		putchar(alphaValue);
+	}
+	putchar('\n');
+	return (0);
+}
