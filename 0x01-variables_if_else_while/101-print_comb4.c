39e4c7392e2429c37e9d401a3d037975e04cbf79
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-19 05:59:28 -0700

    Number combination 3

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+  * main - Print possible combinations 
+  *        of numbers
+  *
+  * Return: always 0(success)
+  */
+int main(void)
+{
+	int a,b,c;
+	for (a = 48; a <= 57; a++)
+	{
+		for ( b = a+1; b <= 56; b++)
+		{
+			for(c = b+1; c <= 57; c++)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if(a != 55 || b!= 56 || c!=57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
