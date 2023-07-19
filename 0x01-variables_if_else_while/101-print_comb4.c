794b2387ac1f20e1e2e51e79b97c8d93eb0d90c2
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-19 06:00:25 -0700

    Number combination 3

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,16 +8,17 @@
 int main(void)
 {
 	int a,b,c;
+
 	for (a = 48; a <= 57; a++)
 	{
 		for ( b = a+1; b <= 56; b++)
 		{
-			for(c = b+1; c <= 57; c++)
+			for (c = b+1; c <= 57; c++)
 			{
 				putchar(a);
 				putchar(b);
 				putchar(c);
-				if(a != 55 || b!= 56 || c!=57)
+				if (a != 55 || b!= 56 || c!=57)
 				{
 					putchar(',');
 					putchar(' ');
