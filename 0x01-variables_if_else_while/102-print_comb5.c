dfe18fbc8f749bccecda4685330b47aed94ec613
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-20 07:09:00 -0700

    print comb 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,19 +4,20 @@
  *
  * Return: always 0
  */
-int main (void)
+int main(void)
 {
 	int i, j;
+
 	for (i = 0; i < 99; i++)
 	{
-		for(j = i+1; j < 100; j++)
+		for (j = i + 1; j < 100; j++)
 		{
-			putchar((i/10) + '0');
+			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
 			putchar(' ');
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
-			if( j != 99 || i != 98)
+			if (j != 99 || i != 98)
 			{
 				putchar(',');
 				putchar(' ');
