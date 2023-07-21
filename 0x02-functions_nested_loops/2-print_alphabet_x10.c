f8d3547f6aab19d00c6a8811bcc87e44211ebc18
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-21 15:51:31 -0700

    Print 10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,12 @@
 #include "main.h"
+/**
+ * print_alphabet_x10 - print 10 lines of the alphabet
+ *
+ * Return: always 0
+ */
 void print_alphabet_x10(void)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 1; i <= 10; i++)
 	{
