e0309563c64dc6ac8dd7e1d6b436b96fa04ba7cb
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-22 14:34:22 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,20 @@
  * _isalpha - print all alphabets
  *
  * @c: represents character to be checked
- * 
+ *
  * Return: 1 if c is an alphabet 0 otherwise
  */
 int _isalpha(int c)
 {
-	if ( c <= 122 && c != 91 && c != 92 && c != 93 && c != 94 && c != 95 && c != 96 && !(c > 122) && !(c < 65))
+	if (c <= 122 &&
+			c != 91 &&
+			c != 92 &&
+			c != 93 &&
+			c != 94 &&
+			c != 95 &&
+			c != 96 &&
+			!(c > 122) &&
+			!(c < 65))
 	{
 		return (1);
 	}
