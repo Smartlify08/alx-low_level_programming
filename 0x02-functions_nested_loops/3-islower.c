f2db6c3263bfb9cbe11f1eac51318a92e223aa39
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-22 07:17:33 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - check if value is lowercase 
+ *            and return 1 otherwise return
+ *            0
+ */
+int _islower(int c)
+{
+	if (c <= 90)
+	{
+		return (0);
+		_putchar(c);
+	}
+	else{
+		return 1;
+	}
+	_putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -3,8 +3,13 @@
 int main (void)
 {
 
-	print_alphabet();
+	/*print_alphabet();
 
 	print_alphabet_x10();
+*/
+	int r;
+	r = _islower('H');
+	_putchar(r + '0');
+	_putchar('\n');
 	return (0);
 }
