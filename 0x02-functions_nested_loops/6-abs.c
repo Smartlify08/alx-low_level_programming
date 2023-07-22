543191daabd86f03c81b3ad64919e0ec34577625
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-22 16:16:16 -0700

    Absolute values

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,17 +1,25 @@
 #include "main.h"
+/** _abs - print absoulte value of numbers
+ *
+ * @x: number to be checked if negative or positive
+ *
+ * Return: x * -1 if x is negative,
+           x * 1 if x is positive,
+ *         0 if x is zero
+ */
 int _abs (int x)
 {
 	if (x < 0)
 	{
-		return x * -1;
+		return (x * -1);
 	}
 	else if (x > 0)
 	{
-		return x * 1;
+		return (x * 1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
-	return 0;
+	return (0);
 }
