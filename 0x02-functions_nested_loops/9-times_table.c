a91c1f72fc0022fab0ade3807c5c376bb4ba13c5
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-24 10:29:10 -0700

    9- times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,9 +10,10 @@ void times_table(void)
 
 	for (number = 0; number <= 9; number++)
 	{
-		for(count = 0; count <= 9; count++)
+		for (count = 0; count <= 9; count++)
 		{
 			int res =(number * count);
+
 			if (res < 10 && count != 0)
 			{
 				_putchar(',');
@@ -28,8 +29,7 @@ void times_table(void)
 			{
 				_putchar(res / 10 + '0');
 			}
-			_putchar(res % 10 + '0');
-	
+                        _putchar(res % 10 + '0');
 		}
 		_putchar('\n');
 	}
