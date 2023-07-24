4a68face84dfe75968f731232ebd57035e50b28d
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-24 10:53:13 -0700

    9- times table

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add two numbers and print sum
+ *
+ * @i: represent character to be added
+ * @j: represent character to be added
+ *
+ * Return: sum of the numbers
+ */
+int add(int i, int j)
+{
+	int sum = i + j;
+	return (sum);
+}
