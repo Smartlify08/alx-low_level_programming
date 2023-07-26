199cd819fd5e8437d31cfdcca7eb6a9ff6906732
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-26 00:30:46 -0700

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,23 +3,24 @@
 /**
  * print_to_98 - prints from a specific number to 98
  *
- * @n - represents number to be printed from
+ * @n: represents number to be printed from
  *
  * Return - none(void function)
  */
 void print_to_98(int n)
 {
 	int placeholder, increment;
+
 	increment = (n < 98) ? 1 : -1;
-
-	for( placeholder = n; placeholder <= 98 || placeholder > 98; placeholder += increment)
+	for (placeholder = n;
+			placeholder <= 98 || placeholder > 98;
+		       	placeholder += increment)
 	{
-
 	if (placeholder != 98)
 		{
-			printf("%d, ", placeholder );
+			printf("%d, ", placeholder);
 		}
-		if(placeholder == 98)
+		if (placeholder == 98)
 		{
 			printf("%d\n", placeholder);
 			break;
