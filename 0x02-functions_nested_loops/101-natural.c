50616d90c52d9b69ead47474e459cbd0ca6c893d
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-27 12:29:12 -0700

    Print sum of multiples

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,15 +1,20 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * main - print sum of multiples of 3 and 5
+ *        between 0 and 1024
+ *
+ * Return: 0(success)
+ */
 int main(void)
 {
 	int n;	
 
 	int sum = 0;
-
-        for ( n = 0; n < 1024; n++)
+	
+	for ( n = 0; n < 1024; n++)
 	{
-                if ((n % 3 == 0 || n % 5 == 0) && n != 0)
+		if ((n % 3 == 0 || n % 5 == 0) && n != 0)
 		{
 			sum = sum + n;
 		}
