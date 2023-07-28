d89da80051b03f2ca65d11fe41179b3c045af00e
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-28 13:26:43 -0700

    50 fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
+/**
+ * main - print first 50 fibonacci numbers
+ * 
+ * Return: 0 (successful)
+ *
+ */
 int main(void)
 {
 	int n;
+
 	unsigned long int firstTerm = 1;
+
 	unsigned long int secondTerm = 2;
+
 	unsigned long int nextTerm;
+
 	int i;
+
 	n = 50;
 	printf("%lu, %lu, ", firstTerm, secondTerm);
 	for (i = 2; i < n; i++)
@@ -13,8 +24,8 @@ int main(void)
 		nextTerm = firstTerm + secondTerm;
 		firstTerm = secondTerm;
 		secondTerm = nextTerm;
-		printf("%lu",secondTerm);
-		if(i < n - 1)
+		printf("%lu", secondTerm);
+		if (i < n - 1)
 		{
 			printf(", ");
 		}
