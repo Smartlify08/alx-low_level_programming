53b062bb4fc932a7c3a8a2a8ed12e20a6a739c75
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-07-30 13:20:16 -0700

    Print 98 fib numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,47 +5,25 @@
  * Return: always 0 (successful)
  *
  */
+
 int main(void)
 {
-	unsigned long int n = 98;
-
-	unsigned long int i;
-
-	unsigned long int firstTerm = 1;
-
-	unsigned long int secondTerm = 2;
-
-	unsigned long int firstTerm1, firstTerm2, secondTerm1, secondTerm2;
-
-	printf("%lu, ", firstTerm);
-	printf("%lu", secondTerm);
-	for (i = 1; i < 91; i++)
+	long unsigned int a = 1;
+	long unsigned int b = 2;
+	long unsigned int c;
+	long unsigned int i;
+	long unsigned int n = 98;
+	printf("%lu, %lu", a, b);
+	for(i = 2; i < n; i++)
 	{
-		secondTerm = secondTerm + firstTerm;
-		firstTerm = secondTerm - firstTerm;
-
-		printf(", %lu", secondTerm); 
-	}
-
-	firstTerm1 = firstTerm / 1000000000;
-	firstTerm2 = firstTerm % 1000000000;
-	secondTerm1 = secondTerm / 1000000000;
-	secondTerm2 = secondTerm % 1000000000;
-
-	for (i = 92; i <= n; i++)
-	{
-		                printf(", %lu",secondTerm1 +
-						(secondTerm2 / 1000000000));
-
-                printf("%lu", secondTerm2 % 1000000000);
-
-                secondTerm1 = secondTerm1 + firstTerm1;
-                firstTerm1 = secondTerm1 - firstTerm1;
-
-
-
-                secondTerm2 = firstTerm2 + secondTerm2;
-                firstTerm2 = secondTerm2 - firstTerm2;
+		c = a + b;
+		a = b;
+		b = c;
+		if(1 < n - 1)
+		{
+			printf(", ");
+		}
+		printf("%lu",b);
 	}
 	printf("\n");
 	return (0);
