8e96bfb8016c1c27d3e7efb63c7216da270c9233
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-08-01 08:47:27 -0700

    0 > 972

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,28 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if ((a > b && a > c) || 
+			(c == b && a > c && a > b))
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if ((b > a && b > c) || 
+			(a == c && b > c && b > a))
 	{
 		largest = b;
 	}
-	else
+	else if ((c > a && c > b) || 
+			(b == a && c > a && c > b))
 	{
 		largest = c;
+	}
+	else if (a == c && a == b)
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
 	}
 	return (largest);
 }
--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -11,12 +11,15 @@ int main(void)
 	int a, b, c;
 	int largest;
 
-	a = 972;
-	b = 978;
-	c = 980;
+	a = 19;
+	b = 19;
+	c = 17;
 
 	largest = largest_number(a, b, c);
-	printf("%d is the largest number\n", largest);
+	if (largest != 0)
+	{
+		printf("%d is the largest number\n", largest);
+	}
 
 	return (0);
 }
