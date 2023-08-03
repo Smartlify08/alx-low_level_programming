fec4a258d36f3c27c828607937694e625ecf1be8
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-08-03 14:46:21 -0700

    2-multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul- multiply two numbers
+ *
+ * @a: represents character to be multiplied
+ *
+ * @b: represents character to be multiplied
+ *
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	return a * b;
+}
