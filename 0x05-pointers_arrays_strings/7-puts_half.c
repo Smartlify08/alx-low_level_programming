cf39a491f1508b075d1d674c88e833fa5b232c8b
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-08-24 15:54:19 -0700

    Print half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,16 +2,34 @@
 
 void puts_half(char *str)
 {
-	int n;
-	int length;
+
 	int i;
+	int length;
 	length = 0;
 
-	for (i = 0; str [i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		length = i;
+	}
+	
+
+
+	if (length % 2 != 0 && length != 0)
+
+	{
+		for (i = ((length - 1) / 2) + 1; i <= length; i++)
+		{
+			_putchar(str[i]);
+		}
+
+	}
+	
+
+	for (i = (length / 2) + 1; i <= length; i++)
+	{
 		_putchar(str[i]);
 	}
+	_putchar('\n');
 
 
 }
