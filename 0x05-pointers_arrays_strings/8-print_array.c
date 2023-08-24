cbd97c60788b8d61223e071989266496379f078a
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-08-24 16:31:01 -0700

    Print array elements into standard input

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{	
+		if (i == n)
+		{
+			break;
+		}
+
+		printf("%d", a[i]); 
+
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+
+
+	}
+	printf("\n");
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 void reset_to_98(int *n);
 void puts_half(char *str);
+void print_array(int *a, int n);
 void puts2(char *str);
 void rev_string(char *s);
 void print_rev(char *s);
