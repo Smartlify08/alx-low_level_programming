6e6a179500e4cd52235accab36ced0827e342ab3
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-08-24 14:18:07 -0700

    Reverse strings without printing

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+void rev_string(char *s)
+{
+	int length;
+	int i;
+	int start;
+	int end;
+	length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length = i;
+	}
+
+	start = 0;
+	end = length;
+
+	if (length != 0)
+	{
+		while (start < end)
+		{
+			s[start] = s[start] ^ s[end];
+			s[end] = s[start] ^ s[end];
+			s[start] = s[start] ^ s[end];
+
+			start++;
+			end--;
+		}
+
+
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 void reset_to_98(int *n);
+void rev_string(char *s);
 void print_rev(char *s);
 void _puts(char *str);
 void swap_int(int *a, int *b);
