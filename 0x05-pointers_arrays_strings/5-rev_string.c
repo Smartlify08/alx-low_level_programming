618ada13fdaf2be865de24a32ca8d371882a7c8e
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-08-24 14:24:05 -0700

    Reverse strings without printing

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,12 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * rev_string - reverse a string 
+ *
+ * @s: represents character to be  reveresed
+ *
+ */
 void rev_string(char *s)
 {
 	int length;
 	int i;
 	int start;
 	int end;
+
 	length = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
