06a798b0286b2032f1689602b391c96e33a1e169
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-09-19 14:42:19 -0700

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * reverse_array - prints the reverse of an array
+ *
+ * @a: represents the array
+ * @n: represents the number of the array
+ *
+ */
+
 void reverse_array(int *a, int n)
 {
 	int start = 0;
