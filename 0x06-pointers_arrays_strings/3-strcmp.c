be5438be716d26b5367c607ee050df18e00ea6ab
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-09-19 13:34:54 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,16 @@
 #include "main.h"
 
+/**
+ * _strcmp - compares two strings and returns integer
+ *
+ * @s1: represents first string
+ * @s2: represents second string
+ *
+ * Return: s1 - s2 if strings are unequal,
+ * return 0 if the are equal.
+ *
+ */
+
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 != '\0' && *s2 != '\0')
