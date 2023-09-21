8407149c8c56178774f81dc5859c19900a4180df
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-09-21 07:13:35 -0700

    Convert to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+char *string_toupper(char *str)
+{
+	while (*str)
+	{
+		if(*str >= 'a' && *str <= 'z')
+		{
+			*str = *str - 'a' + 'A';
+		}
+		str++;
+	}
+
+	return (str);
+}
