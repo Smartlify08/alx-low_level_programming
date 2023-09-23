cba169fb42df15714e954dc21d65a3e21a25e744
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-09-23 16:09:53 -0700

    Fixed betty errors

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,8 +1,18 @@
 #include "main.h"
 
+/**
+ * _strncpy - copy content of a string to another
+ * @dest: destination string
+ * @src: source string
+ * @n: limit
+ * Return: original destination string
+ */
+
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	char *original_dest = dest;
+
 	for (; *src && n > 0;)
 	{
 		*dest = *src;
