6d03eb1834e7fc15539dae944ed188075d7b367f
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-09-25 04:49:40 -0700

    Fixed betty errors

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,17 +1,23 @@
 #include "main.h"
 
+/**
+ * reverse_array - reverses an array
+ * @a: array
+ * @n: reverse limit
+ */
 
 void reverse_array(int *a, int n)
 {
 	int start;
-	int end;
-	int *length = &n;
 
+	int end;
+
+	int *length = &n;
 
 	start = 0;
 	end = *length - 1;
 
-	for(; start < end;)
+	for (; start < end;)
 	{
 		int storeStart = a[start];
 
