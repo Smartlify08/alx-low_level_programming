43a991ae1d41873daa1298d5ca547b2e25404bd1
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-10-01 09:59:49 -0700

    Fixed betty errors

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * factorial - calculates factorial of a number
+ * @n: number
+ * Return: - 1 for n < 0, 1 for n <= 1,
+ * factorial(n-1) * n for n > 1
+**/
+
 int factorial(int n)
 {
 	if (n < 0)
