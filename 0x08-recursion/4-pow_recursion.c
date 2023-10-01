fcab444288bf3b45aac79beeda58045a7029af65
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-10-01 12:57:55 -0700

    Fixed betty errors

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _pow_recursion - returns power of a number
+ * @x: base number
+ * @y: power
+ * Return : 1, -1 or x * _pow_recursion(x, y - 1))
+**/
+
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
