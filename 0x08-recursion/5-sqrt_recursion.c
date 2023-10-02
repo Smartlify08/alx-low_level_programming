2f2982f66f1123f53ccc5e5962ac544707cbf529
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-10-02 02:12:06 -0700

    Fixed betty errors

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,19 +1,26 @@
 #include "main.h"
 
+/**
+ * do_sqrt - calculates the square root of the number
+ * @i: iterates through n
+ * @n: number
+ * Return: i, -1, or do_sqrt(i+1,n)
+**/
+
 int do_sqrt(int i, int n)
 {
 	int result;
+
 	if (i > n)
 	{
 		return (-1);
 	}
-	
 
 
 	result = i * i;
 	if (result == n)
 	{
-		return(i);
+		return (i);
 	}
 
 	else if (result > n)
@@ -23,6 +30,14 @@ int do_sqrt(int i, int n)
 	return (do_sqrt(i + 1, n));
 
 }
+
+
+/**
+ * _sqrt_recursion - returns do_sqrt function
+ * @n: number
+ * Return: -1, n, do_sqrt
+**/
+
 
 int _sqrt_recursion(int n)
 {
