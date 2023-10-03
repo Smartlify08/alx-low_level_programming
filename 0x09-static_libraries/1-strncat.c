5d6a97ca4fb2ecc0e13e1f4780d6a7e8d81adc72
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-10-03 16:13:15 -0700

    Added object files

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,1 +1,29 @@
+#include "main.h"
 
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: limit
+ * Return: original destination string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *original_dest = dest;
+
+	for (; *dest; dest++)
+	{
+
+	}
+
+	for (; *src && n > 0; src++, dest++, n--)
+	{
+		*dest = *src;
+	}
+
+	*dest = '\0';
+
+	return (original_dest);
+}
