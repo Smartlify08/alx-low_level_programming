3485594a688ffcf8f89750735cc45777122df0dd
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-10-04 15:10:55 -0700

    Fixed betty errors

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -12,27 +12,24 @@ int _atoi(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-	    if (s[i] == '+')
-	    {
-		sign = 1;
-		i++;
-	    }
-
-	    else if (s[i] == '-')
-	    {
-		sign = -1;
-		i++;
-	    }
-
-	    else if (s[i] == ' ' || s[i] == '\t')
-	    {
-		i++;
-	    }
-
-	    if (s[i] >= 48 && s[i] <= 57)
-	    {
-		result = result * 10 + s[i] - 48;
-	    }
+		if (s[i] == '+')
+		{
+			sign = 1;
+			i++;
+		}
+		else if (s[i] == '-')
+		{
+			sign = -1;
+			i++;
+		}
+		else if (s[i] == ' ' || s[i] == '\t')
+		{
+			i++;
+		}
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			result = result * 10 + s[i] - 48;
+		}
 	}
 
 	return (result * sign);
