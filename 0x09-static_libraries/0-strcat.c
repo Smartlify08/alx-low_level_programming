77ad1c89be4762ffabec79b55340679702cec661
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-10-04 00:51:06 -0700

    Updated library

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,1 +1,25 @@
+#include "main.h"
 
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * Return: original destination string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	char *original_dest = dest;
+
+	for (; *dest; dest++)
+	{
+
+	}
+
+	for (; *src; dest++, src++)
+	{
+		*dest = *src;
+	}
+
+	return (original_dest);
+}
