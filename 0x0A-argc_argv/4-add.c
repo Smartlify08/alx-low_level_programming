c56838aa5def4c537da620ba40b8d915cc9c7670
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-10-05 15:13:16 -0700

    Fixed betty issues

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,9 +2,17 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+/**
+ * main - add infinite arguments
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 1 unsuccessful, 0 successful
+**/
+
 int main(int argc, char *argv[])
 {
 	int i, j, sum;
+
 	sum = 0;
 
 	for (i = 1; i < argc; i++)
