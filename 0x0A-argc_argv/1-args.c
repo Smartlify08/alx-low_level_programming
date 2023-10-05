ac39f6ffc188a129e3acd5f733038dbf551389cc
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-10-05 06:36:16 -0700

    Fixed betty errors

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - print number of arguments
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 successful
+**/
+
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
