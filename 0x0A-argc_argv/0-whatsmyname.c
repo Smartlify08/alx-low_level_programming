da42deeb463abc9e448d414d3f39bd91a21efa04
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-10-05 06:20:50 -0700

    Fixed betty issues

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - prints name of program
+ * @argc: argument count
+ * @argv: pointer to array of strings
+ * Return: 0 successful
+**/
+
 int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s", argv[i]);
