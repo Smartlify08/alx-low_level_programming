be877f789243ce2ba75e3d9f3630bff78351506a
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-10-06 15:01:16 -0700

    Strdup (first commit)

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *_strdup(char *str)
+{
+	int len, i;
+
+	char *duplicateStr;
+	len = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (len = 0; str[len]; len++)
+	{
+
+	}
+
+	duplicateStr = malloc(len + 1);
+
+	for (i = 0; i < len; i++)
+	{
+		duplicateStr[i] = *(str++);
+	}
+
+	if (duplicateStr == NULL)
+	{
+		return (NULL);
+	}
+
+	return (duplicateStr);
+}
