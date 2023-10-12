3823ce8bb4c03a6f6110b18b10972e0181f518ae
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-10-12 02:05:33 -0700

    Fixed betty errors

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,10 +3,17 @@
 #include "main.h"
 
 
+/**
+ * array_range - creates a range of array pf integers
+ * @min: minimum element of arrays
+ * @max: maximum element in array
+ * Return: NULL, ptr(success)
+**/
+
 int *array_range(int min, int max)
 {
 	int *ptr, i, j;
-	
+
 	i = 0;
 	j = max + 1 - min;
 
