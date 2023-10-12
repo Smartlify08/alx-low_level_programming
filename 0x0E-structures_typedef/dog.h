cab87026230c79bb1283bf362d56e6f6373a0f9c
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-10-12 05:39:51 -0700

    Documented struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,21 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+
+/**
+ * struct dog - structure of a dog
+ *
+ * creates a name for the dog, an age and an owner
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+*/
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif /* __DOG_H__ */
